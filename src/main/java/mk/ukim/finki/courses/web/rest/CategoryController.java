3f04d5b57f51a7105b5a0ee474856c1efc6a0b84
Nace Gjorgjievski <nace.gorgievski123@gmail.com>
2024-05-26 21:46:18 +0200

    update cors

--- src/main/java/mk/ukim/finki/courses/web/rest/CategoryController.java
+++ src/main/java/mk/ukim/finki/courses/web/rest/CategoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://courses-app-umkt.onrender.com")
 @RequestMapping("/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
--- src/main/java/mk/ukim/finki/courses/web/rest/CommentController.java
+++ src/main/java/mk/ukim/finki/courses/web/rest/CommentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://courses-app-umkt.onrender.com")
 @RequestMapping("/api/comments")
 public class CommentController {
 
